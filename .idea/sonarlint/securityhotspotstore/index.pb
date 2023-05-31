2859a74fc66a171f4382bc5410ae250fe7af5ea9
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-05-31 14:17:27 +0200

    Add Java Intro

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+H
+src/main/java/Intro.java,8/9/8947ff27d7a898dafc7ecc14a0a85648f961b8f6
---
+++ src/main/java/Intro.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+public class Intro {
+    public static void main(String[] args) {
+        String myFirstStringVariable = "Hallo cgn-23-2!!!";
+
+        System.out.println(myFirstStringVariable);
+
+        myFirstStringVariable = "Hallo Welt!" + " !!!";
+
+        System.out.println(myFirstStringVariable);
+
+
+        int myFirstIntegerValue = 3;
+        int mySecondIntegerValue = 3 * myFirstIntegerValue;
+        System.out.println("Summe: " + (3 + 3) );
+
+        boolean myFirstBooleanValue = true;
+        boolean mySecondBooleanValue = false || true;
+
+        char myFirstChar = 'A';
+
+
+        float myFirstDoubleValue = 0.2f;
+        float mySecondDoubleValue = 0.7f;
+
+        System.out.println(myFirstDoubleValue + mySecondDoubleValue);
+
+        System.out.println(myFirstIntegerValue >= 1);
+
+        String name1 = "Florian";
+        String name2 = "Christian";
+        String name3 = "Ammar";
+        String name4 = "Collins";
+        String name5 = "Sahed";
+
+        String[] names = new String[6];
+        names[0] = name1;
+        names[1] = name2;
+        names[2] = name3;
+        names[3] = name4;
+        names[4] = name5;
+
+        System.out.println(names[5]);
+
+
+
+
+        int[] numbers = new int[9453];
+        numbers[0] = 123;
+        numbers[1] = 1;
+        numbers[2] = 6874841;
+
+        System.out.println(numbers[0] + " " + numbers[2]);
+
+        System.out.println(numbers[0] + numbers[1]);
+
+        System.out.println(numbers.length);
+
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
+}
