cea8908d114cfd9afc2d885455726f466ba0303f
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-05-31 15:15:32 +0200

    Add Joshua

--- src/main/java/Intro.java
+++ src/main/java/Intro.java
@@ -32,7 +32,7 @@ public class Intro {
         String name2 = "Christian";
         String name3 = "Daniel";
         String name4 = "Anton";
-        String name5 = "Sahed";
+        String name5 = "Joshua";
 
         String[] names = new String[6];
         names[0] = name1;
